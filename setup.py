0b4329f69cbef127daeb1e2d9477bb24d5c47623
Neha Vishwakarma <nehavishwakarma7777@gmail.com>
2023-08-11 13:04:38 +0530

    update

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
 ## Declaring variable for setup funtion
 PROJECT_NAME = "housing-predictor"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 AUTHOR = "Neha Vishwakarma" 
 DESCRIPTION = "This is My Full end to end project."
-PACKAGES =["housing"]
+## It will find where is __init__function and add them as a package
+PACKAGES = find_packages()   # ['housing']
 REQUIREMENT_FILE_NAME ="requirements.txt"
 
 def get_requirements_list()->List[str]:
@@ -19,7 +20,7 @@ def get_requirements_list()->List[str]:
     mentioned in requirements.txt file
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -30,7 +31,4 @@ setup(
     packages=PACKAGES,
     install_requires = get_requirements_list()
 )
-"""
-if __name__ =="__main__":
-    print(get_requirements_list())
-"""
+## housing is package and __init__ is module
